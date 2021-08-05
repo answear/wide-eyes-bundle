ae58a383cfd7f06196409b14b6ffe85fe22c0832
Joanna Rawska <joanna.rawska@wearco.pl>
2021-08-05 08:35:06 +0200

    Add many async requests for SimilarRequest

--- src/Service/AbstractClient.php
+++ src/Service/AbstractClient.php
@@ -9,6 +9,8 @@ use Answear\WideEyesBundle\Exception\ServiceUnavailable;
 use Answear\WideEyesBundle\Request\Request;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Promise;
+use Psr\Http\Message\ResponseInterface;
 use Webmozart\Assert\Assert;
 
 abstract class AbstractClient
@@ -33,14 +35,59 @@ abstract class AbstractClient
                     'body' => $request->toJson(),
                 ]
             );
-            if ($response->getBody()->isSeekable()) {
-                $response->getBody()->rewind();
-            }
-            $responseText = $response->getBody()->getContents();
+            $responseText = $this->getResponseText($response);
         } catch (GuzzleException $e) {
             throw new ServiceUnavailable($e->getMessage(), $e->getCode(), $e);
         }
 
+        return $this->getResult($responseText);
+    }
+
+    /**
+     * @param Request[] $requests
+     */
+    protected function manyRequests(string $endpoint, array $requests): array
+    {
+        $promises = [];
+
+        foreach ($requests as $key => $request) {
+            $promises[$key] = $this->guzzle->requestAsync(
+                'POST',
+                $endpoint,
+                [
+                    'headers' => $this->configProvider->getRequestHeaders(),
+                    'body' => $request->toJson(),
+                ]
+            );
+        }
+
+        $promisesResults = Promise\settle($promises)->wait();
+
+        $processedResults = [];
+        foreach ($promisesResults as $key => $result) {
+            if ('fulfilled' === $result['state']) {
+                try {
+                    $processedResults[$key] = $this->getResult($this->getResponseText($result['value']));
+                } catch (MalformedResponse $e) {
+                    $processedResults[$key] = [];
+                }
+            }
+        }
+
+        return $processedResults;
+    }
+
+    private function getResponseText(ResponseInterface $response): string
+    {
+        if ($response->getBody()->isSeekable()) {
+            $response->getBody()->rewind();
+        }
+
+        return $response->getBody()->getContents();
+    }
+
+    private function getResult(string $responseText): array
+    {
         $decoded = \json_decode($responseText, true);
 
         try {
--- tests/Unit/Service/SimilarClientTest.php
+++ tests/Unit/Service/SimilarClientTest.php
@@ -12,6 +12,7 @@ use GuzzleHttp\Psr7\Response;
 class SimilarClientTest extends AbstractClientTest
 {
     private const UID = 'uid1';
+    private const SECOND_UID = 'uid5';
     private const COUNTRY_CODE = 'code';
 
     private SimilarClient $client;
@@ -102,6 +103,64 @@ class SimilarClientTest extends AbstractClientTest
         $this->expectException(ServiceUnavailable::class);
 
         $this->client->getSimilar(self::UID, self::COUNTRY_CODE);
+    }
+
+    /**
+     * @test
+     */
+    public function successfulManySimilarRequest(): void
+    {
+        $resultUids = ['uid2', 'uid3', 'uid4'];
+        $resultUids2 = ['uid2', 'uid3'];
+
+        $this->guzzleHandler->append(
+            new Response(200, [], $this->prepareProperResponse($resultUids)),
+            new Response(200, [], $this->prepareProperResponse($resultUids2))
+        );
+
+        $result = $this->client->getSimilarForMany([self::UID, self::SECOND_UID], self::COUNTRY_CODE);
+
+        self::assertSame($resultUids, $result[self::UID]->getUids());
+        self::assertSame($resultUids2, $result[self::SECOND_UID]->getUids());
+        self::assertCount(2, $this->guzzleHistory);
+    }
+
+    /**
+     * @test
+     */
+    public function successfulManySimilarRequestWithOne500(): void
+    {
+        $resultUids = ['uid2', 'uid3', 'uid4'];
+
+        $this->guzzleHandler->append(
+            new Response(200, [], $this->prepareProperResponse($resultUids)),
+            new Response(500, [], '{}')
+        );
+
+        $result = $this->client->getSimilarForMany([self::UID, self::SECOND_UID], self::COUNTRY_CODE);
+
+        self::assertSame($resultUids, $result[self::UID]->getUids());
+        self::assertArrayNotHasKey(self::SECOND_UID, $result);
+        self::assertCount(2, $this->guzzleHistory);
+    }
+
+    /**
+     * @test
+     */
+    public function successfulManySimilarRequestWithOneErrorResult(): void
+    {
+        $resultUids = ['uid2', 'uid3', 'uid4'];
+
+        $this->guzzleHandler->append(
+            new Response(200, [], $this->prepareProperResponse($resultUids)),
+            new Response(200, [], '{"success":true}')
+        );
+
+        $result = $this->client->getSimilarForMany([self::UID, self::SECOND_UID], self::COUNTRY_CODE);
+
+        self::assertSame($resultUids, $result[self::UID]->getUids());
+        self::assertSame([], $result[self::SECOND_UID]->getUids());
+        self::assertCount(2, $this->guzzleHistory);
     }
 
     private function prepareProperResponse(array $resultUids): string
